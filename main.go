911a3fc4d9b6aa47cb8558a64365a16a31979178
Jimi Werwath <jim.werwath@uptake.com>
2019-09-17 22:35:43 -0500

    stait4

--- main.go
+++ main.go
@@ -16,6 +16,50 @@ func (a Hand) Len() int           { return len(a) }
 func (a Hand) Swap(i, j int)      { a[i], a[j] = a[j], a[i] }
 func (a Hand) Less(i, j int) bool { return a[i].suit < a[j].suit || a[i].rank < a[i].rank }
 
+// Three returns the rank for any 3 of a kind, -1 if none/error
+func (a Hand) Three() int {
+	if a == nil {
+		return -1
+	}
+
+	var trios = [10][3]int{
+		{0, 1, 2},
+		{0, 1, 3},
+		{0, 1, 4},
+		{0, 2, 3},
+		{0, 2, 4},
+		{0, 3, 4},
+		{1, 2, 3},
+		{1, 2, 4},
+		{1, 3, 4},
+		{2, 3, 4},
+	}
+
+	for t := 0; t < 10; t++ {
+		if a[trios[t][0]].rank == a[trios[t][1]].rank &&
+			a[trios[t][0]].rank == a[trios[t][2]].rank {
+			return a[trios[t][0]].rank
+		}
+	}
+
+	return -1
+}
+
+//Strait4 returns the suit and rank of a straight. -1 for none
+func (a Hand) Strait4() (int, int) {
+	for start := 0; start < len(a)-3; start++ {
+		if a[start].suit == a[start+1].suit &&
+			a[start].suit == a[start+2].suit &&
+			a[start].suit == a[start+3].suit &&
+			a[start].rank+1 == a[start+1].rank &&
+			a[start].rank+2 == a[start+2].rank &&
+			a[start].rank+3 == a[start+3].rank {
+			return a[start].suit, a[start].rank
+		}
+	}
+	return -1, -1
+}
+
 // NewCard replaces the nth card with a new one from the deck
 func (h Hand) NewCard(n int, d *Deck) error {
 	if n >= len(h) {
--- main_test.go
+++ main_test.go
@@ -6,6 +6,77 @@ import (
 	"testing"
 )
 
+var (
+	fourOfAKind = Hand([]Card{
+		Card{
+			suit: 0,
+			rank: 0,
+		},
+		Card{
+			suit: 0,
+			rank: 1,
+		},
+		Card{
+			suit: 1,
+			rank: 1,
+		},
+		Card{
+			suit: 2,
+			rank: 1,
+		},
+		Card{
+			suit: 3,
+			rank: 1,
+		},
+	})
+
+	threeOfAKind = Hand([]Card{
+		Card{
+			suit: 0,
+			rank: 0,
+		},
+		Card{
+			suit: 0,
+			rank: 1,
+		},
+		Card{
+			suit: 1,
+			rank: 1,
+		},
+		Card{
+			suit: 2,
+			rank: 1,
+		},
+		Card{
+			suit: 3,
+			rank: 2,
+		},
+	})
+
+	fourStrait = Hand([]Card{
+		Card{
+			suit: 3,
+			rank: 3,
+		},
+		Card{
+			suit: 1,
+			rank: 1,
+		},
+		Card{
+			suit: 1,
+			rank: 2,
+		},
+		Card{
+			suit: 1,
+			rank: 3,
+		},
+		Card{
+			suit: 1,
+			rank: 4,
+		},
+	})
+)
+
 func TestDeck(t *testing.T) {
 	deck1 := NewDeck()
 	deck2 := NewDeck()
@@ -44,6 +115,7 @@ func TestOverDraw(t *testing.T) {
 	if err != nil {
 		t.Errorf(err.Error())
 	}
+
 	if 52 != len(cards) {
 		t.Errorf("draw whole desk is %d", len(cards))
 	}
@@ -52,7 +124,6 @@ func TestOverDraw(t *testing.T) {
 	if err == nil {
 		t.Errorf("expected overdraw")
 	}
-
 }
 
 func TestSortHand(t *testing.T) {
@@ -71,5 +142,39 @@ func TestSortHand(t *testing.T) {
 
 	if 1 != hand[0].suit || 2 != hand[0].rank {
 		t.Errorf("sort failed %v", hand)
+	}
+}
+
+func TestThree(t *testing.T) {
+	if 1 != threeOfAKind.Three() {
+		t.Errorf("threeOfAKind")
+	}
+	if 1 != fourOfAKind.Three() {
+		t.Errorf("threeOfAKind")
+	}
+	if -1 != fourStrait.Three() {
+		t.Errorf("threeOfAKind")
+	}
+}
+
+func TestStrait4(t *testing.T) {
+
+	for _, c := range []struct {
+		hand Hand
+		suit int
+		rank int
+		desc string
+	}{
+		{hand: threeOfAKind, suit: -1, rank: -1, desc: "t1"},
+		{hand: fourOfAKind, suit: -1, rank: -1, desc: "t2"},
+		{hand: fourStrait, suit: 1, rank: 1, desc: "t3"},
+	} {
+		suit, rank := c.hand.Strait4()
+		if c.rank != rank {
+			t.Errorf("wrong rank %d expect %d %s", rank, c.rank, c.desc)
+		}
+		if c.suit != suit {
+			t.Errorf("wrong suit %d expect %d  %s", suit, c.suit, c.desc)
+		}
 	}
 }
